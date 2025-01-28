6dc7ae9cc40911fe0c99f7f45d9a3c281a1bb0c1
arushi <arshibha2019@gmail.com>
2025-01-28 20:21:34 +0530

    changes to main1.tf

--- Desktop/terraforms3object/main1.tf
+++ Desktop/terraforms3object/main1.tf
@@ -12,6 +12,8 @@ provider "aws"{
     region = "us-east-1"
     access_key = "AKIAW2EEW3ZVHJ2YVMD"
     secret_key = "9Ne6d7ermvg6ueymY3KoNfZynfrB9Pn3ls9sxY8"
+
+    git 
 }
 
 resource "aws_s3_bucket" "mybucket"{
